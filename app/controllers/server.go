0ee52866d48de068fa5099f7439dc9408e15a0cc
yuita-yoshihiko <soup998211@gmail.com>
2023-07-29 21:17:40 +0900

    140まで

--- app/controllers/server.go
+++ app/controllers/server.go
@@ -3,9 +3,27 @@ package controllers
 import (
 	"net/http"
 	"todo_app/config"
+	"fmt"
+	"html/template"
 )
 
+func generateHTML(w http.ResponseWriter, data interface{}, filenames ...string) {
+	var files []string
+	for _, file := range filenames {
+		files = append(files, fmt.Sprintf("app/views/templates/%s.html", file))
+	}
+
+  templates := template.Must(template.ParseFiles(files...))
+	templates.ExecuteTemplate(w, "layout", data)
+}
+
 func StartMainServer() error {
+	files := http.FileServer(http.Dir(config.Config.Static))
+	http.Handle("/static/", http.StripPrefix("/static/", files))
+
 	http.HandleFunc("/", top)
+	http.HandleFunc("/signup", signup)
+	http.HandleFunc("/login", login)
+	http.HandleFunc("/authenticate", authenticate)
 	return http.ListenAndServe(":"+config.Config.Port, nil)
 }
--- main.go
+++ main.go
@@ -2,12 +2,13 @@ package main
 
 import (
 	"fmt"
-	"todo_app/app/controllers"
 	"todo_app/app/models"
+	"todo_app/app/controllers"
 )
 
 func main() {
 	fmt.Println(models.Db)
 
 	controllers.StartMainServer()
+
 }
